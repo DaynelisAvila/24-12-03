ea08c98d613b6e4e23a1893da1eb4b51e7f8bb13
DaynelisAvila <avilacumaredaynelis@gmail.com>
2024-01-03 20:25:55 -0500

    primer cambio

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// const listaDeNumero= [2, 3, 5, 7, 11];
+
+// console.log(listaDeNumero[0]);
+// console.log(listaDeNumero[4]);
+
+/* let numeroImpares = [1, 3, 5, 7,];
+cantidad = numeroImpares.length;
+console.log("Primera forma:")
+for(i = 0; cantidad > i; i++){
+    console.log(numeroImpares[i]);
+}
+console.log("Segunda forma:")
+for(const valor of numeroImpares) {
+    console.log(valor);
+} */
+
+// const listaDeNumero= [2, 3, 5, 7, 11,];
+// listaDeNumero[2] = 13;
+// console.log(listaDeNumero[2]);
+
+// // listaDeNumero = [];
+// listaDeNumero[0] = 100;
+// console.log(listaDeNumero[0]);
+// listaDeNumero[1] = 102;
+// listaDeNumero[2] = -10;
+// listaDeNumero[3] = 23;
+// listaDeNumero[4] = -3;
+// console.log(listaDeNumero[2]);
+
+const mixedArray = [3, 'hola mundo', 3627.5];
+cantidad = mixedArray.length;
+for(i = 0; cantidad > i; i++){
+    console.log(mixedArray[i]);
+}
+
+let questions = [
+    ['¿Cual es la capital de Peru?', 'Lima'],
+    ['¿Cuantos millonarios peruanos somos?', 33],
+    ['¿Cual es la moneda de Peru?', 'soles'],
+]
+
+console.log(questions[0][0]);
+console.log(questions[0][1]);
+console.log(questions[1][0]);
+console.log(questions[1][1]);
+console.log(questions[2][0]);
+console.log(questions[2][1]);
+  
